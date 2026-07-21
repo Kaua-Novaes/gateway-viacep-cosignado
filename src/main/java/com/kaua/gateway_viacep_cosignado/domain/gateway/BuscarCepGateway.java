e3db6c3a6bcefafde7f30d418f139772e098a67e
Kaua-Novaes <kaua88novaes@gmail.com>
2026-07-20 21:34:03 -0300

    feat: adicionando DTOs para o recebimento e envio do payload da API do viacep

---
+++ src/main/java/com/kaua/gateway_viacep_cosignado/domain/gateway/BuscarCepGateway.java
@@ -0,0 +1,4 @@
+package com.kaua.gateway_viacep_cosignado.domain.gateway;
+
+public interface BuscarCepGateway {
+}
---
+++ src/main/java/com/kaua/gateway_viacep_cosignado/dto/viacep/ReturnDto.java
@@ -0,0 +1,4 @@
+package com.kaua.gateway_viacep_cosignado.dto.viacep;
+
+public class ReturnDto {
+}
