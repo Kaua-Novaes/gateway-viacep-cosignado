7aa7ea23148f9336355b1bfcfe5ee8a11dbf9a1f
Kaua-Novaes <kaua88novaes@gmail.com>
2026-07-20 21:05:41 -0300

    feat: estrutura inicial do projeto

---
+++ src/main/java/com/kaua/gateway_viacep_cosignado/GatewayViacepCosignadoApplication.java
@@ -0,0 +1,13 @@
+package com.kaua.gateway_viacep_cosignado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayViacepCosignadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayViacepCosignadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaua/gateway_viacep_cosignado/GatewayViacepCosignadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaua.gateway_viacep_cosignado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayViacepCosignadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
