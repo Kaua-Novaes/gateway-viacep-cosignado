99c65131f1f02ee92fdab5ce5c3e6c6f135fe709
Kaua-Novaes <kaua88novaes@gmail.com>
2026-07-22 19:12:41 -0300

    feat: adição de todos os testes de qualidade, cobrindo todas as camadas da aplicação

--- src/main/java/com/kaua/gateway_viacep_cosignado/config/RestClientConfig.java
+++ src/main/java/com/kaua/gateway_viacep_cosignado/config/RestClientConfig.java
@@ -3,6 +3,7 @@ package com.kaua.gateway_viacep_cosignado.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -13,8 +14,14 @@ public class RestClientConfig {
             RestClient.Builder builder,
             @Value("${integrations.viacep.base-url}") String baseUrl
     ) {
+
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(5000);
+        factory.setReadTimeout(5000);
+
         return builder
                 .baseUrl(baseUrl)
+                .requestFactory(factory)
                 .build();
     }
 }
